310edca0034e9be433bb82a4653c8ad55bbebf90
NikaBotchorishvili <nikabotchorishvili@gmail.com>
2022-03-24 20:12:15 +0400

    better version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,28 @@
+const clock = document.getElementById("clock");
 
-const clock = document.getElementById("clock")
+const displayTime = () => {
+	let d = new Date();
+	let hours = d.getHours();
+	let minutes = d.getMinutes();
+	let seconds = d.getSeconds();
 
-const displayTime = () =>{
-    let d = new Date();
-    let hours = d.getHours();
-    let minutes = d.getMinutes();
-    let seconds = d.getSeconds();
-
-    if(hours < 10){
-        hours = "0" + hours;
-    }
-    if(minutes < 10){
-        minutes = "0" + minutes;
-    }
-    if(seconds < 10){
-        seconds = "0" + seconds;
-    }
-    let time = `${hours}:${minutes}:${seconds}`
-    if(hours < 12){
-        time +=  " AM"
-    }else{
-        time += " PM"
-    }
-    clock.innerHTML = time;
-    setTimeout(displayTime, 1000);
-}
-
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	let time = `${hours}:${minutes}:${seconds}`;
+	if (hours < 12) {
+		time += " AM";
+	} else {
+		time += " PM";
+	}
+	clock.innerHTML = time;
+	
+};
 displayTime();
-
+setInterval(displayTime, 1000)
